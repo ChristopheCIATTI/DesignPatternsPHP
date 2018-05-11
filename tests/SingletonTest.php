49d469f42222f30c20cbdaf464f2d2b7aa816b10
ChristopheCIATTI <37122204+ChristopheCIATTI@users.noreply.github.com>
2018-05-11 10:56:45 +0200

    Tests files

---
+++ tests/SingletonTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DesignPattern\CreationalPatterns\Singleton;
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * 
+ * @author Christophe
+ *
+ */
+class SingletonTest extends TestCase
+{
+    public function testGetInstance()
+    {
+        $singletonA = Singleton::getInstance();
+        $singletonB = Singleton::getInstance();
+        
+        $this->assertInstanceOf(Singleton::class, $singletonA);
+        $this->assertInstanceOf(Singleton::class, $singletonB);
+        $this->assertSame($singletonA, $singletonB);
+    }
+}
